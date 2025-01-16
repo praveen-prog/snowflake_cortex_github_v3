67158e08bd905bd272f957e2d8907c70cb734a2b
Praveen Kumar Chandran <praveenhome@Praveens-MacBook-Pro.local>
2025-01-16 12:53:34 -0500

    updated code

--- src/prompt.py
+++
@@ -1,6 +0,0 @@
-
-chatbot_system_prompt = f"""
-    You are an expert assistant extracting information from context provided.
-    Answer the question based on the context. Be concise and do not hallucinate.
-    If you don't have the information just say so.
-"""
