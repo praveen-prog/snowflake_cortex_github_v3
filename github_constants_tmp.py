739f07139cd70d51e1e3703e8103e11d203c3ec1
Praveen Kumar Chandran <praveenhome@Praveens-MacBook-Pro.local>
2025-01-16 13:25:51 -0500

    Test version

--- github_constants_tmp.py
+++ github_constants_tmp.py
@@ -11,9 +11,9 @@ load_dotenv()
 @dataclass
 class SetUpGitHubClass:
     GITHUB_TOKEN : str = os.environ.get("GITHUB_TOKEN")
-    GITHUB_REPO_LINK : str = "https://api.github.com/repos/praveen-prog/reltiosqlchatbot/branches/main"
+    GITHUB_REPO_LINK : str = "https://api.github.com/repos/praveen-prog/docs/branches/main"
     GITHUB_OWNER : str = "praveen-prog"
-    GITHUB_REPO_NAME: str = "reltiosqlchatbot"
+    GITHUB_REPO_NAME: str = "docs"
     GITHUB_REPO_BRANCH: str = "main"
     GITHUB_FILTER_DIRECTORIES : ClassVar[list[str]] = ["content"]
     GITHUB_FILTER_EXTENSIONS : ClassVar[list[str]] = [".md", ".py"]
